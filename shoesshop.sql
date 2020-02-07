df9889222049cda811c079a2b9d7a0893cee86eb
noname21062000 <noname21062000@gmail.com>
2020-02-07 13:26:38 +0700

    update sql

--- shoesshop.sql
+++ shoesshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 07, 2020 lúc 07:16 AM
+-- Thời gian đã tạo: Th2 07, 2020 lúc 07:26 AM
 -- Phiên bản máy phục vụ: 10.4.11-MariaDB
 -- Phiên bản PHP: 7.4.1
 
