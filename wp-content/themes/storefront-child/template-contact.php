10334894eb86d99613b44c2ee1afc7d9ba6c11e1
noname21062000 <noname21062000@gmail.com>
2020-02-08 20:58:13 +0700

    edit page.php

--- wp-content/themes/storefront-child/template-contact.php
+++ wp-content/themes/storefront-child/template-contact.php
@@ -24,7 +24,7 @@ get_header(); ?>
                                     <div class="note text-center">Fields marked with (*) are required</div>
         
                             </div>
-                            <?php do_action('[contact-form-7 id="138" title="Contact"]')?>
+                            <?php echo do_shortcode('[contact-form-7 id="138" title="Contact"]')?>
                             <!-- <div class="row">
                                 <div class="large-3 medium-4 columns">
                                     <label class="inline">Name <span class="note">(*)</span></label>
