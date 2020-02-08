be02c8d13a85c9b0df1fd5a35c84b94527c34ef0
noname21062000 <noname21062000@gmail.com>
2020-02-08 11:51:14 +0700

    create sidebar.php

---
+++ wp-content/themes/storefront-child/sidebar.php
@@ -0,0 +1,33 @@
+<div id="search_panel">
+		    <div class="panel_body">
+		        <form action="http://nsvn.vn/vn/search.html">
+		            <input type="text" name="kw" value="" placeholder="Nhập từ khóa tìm kiếm...">
+		            <button type="submit" title="Tìm sản phẩm"><i class="fa fa-search" aria-hidden="true"></i></button>
+		        </form>
+		    </div>
+		</div>
+        <div id="support_online" class="panel hidden_mobile">
+		    <h3 class="panel_header">
+		            <i class="fa fa-comments-o"></i>
+		            Hỗ trợ trực tuyến        </h3>
+
+		    <div class="panel_body">
+		        <div class="account rows">
+		            <p class="name">Ms Trancy</p>
+		            <a target="_blank" href="skype:Sandy.nguyen14?chat" rel="nofollow">
+		                <img class="sky" src="http://nsvn.vn/template/img/skype.png" alt="Sandy.nguyen14" width="30px">
+		            </a>
+		            <div class="phone">+84 93 448 93 29</div>
+		        </div>
+		        <!-- account -->
+		        <div class="account rows">
+		            <p class="name">Ms Thasa</p>
+		            <a target="_blank" href="skype:nguyen.thsa2301?chat" rel="nofollow">
+		                <img class="sky" src="http://nsvn.vn/template/img/skype.png" alt="nguyen.thsa2301" width="30px">
+		            </a>
+		            <div class="phone">0084 902 819 638</div>
+		        </div>
+		        <!-- account -->
+		    </div>
+		    <!-- panel_body -->
+		</div>
