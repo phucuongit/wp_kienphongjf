ade16c06d3f4bafc54a7d13547f32d85fdcdbc66
ancao2504 <pullnolovesmile@gmail.com>
2020-02-08 11:49:05 +0700

    create file sidebar.php

--- wp-content/themes/storefront-child/category.php
+++ wp-content/themes/storefront-child/category.php
@@ -51,89 +51,9 @@
     </div>
 
     <div class="news-list-content-right col-lg-3 col-md-12">
-        <div id="search-panel">
-            <div class="panel-body">
-                <form action="http://nsvn.vn/vn/search.html">
-                    <input type="text" name="kw" value="" placeholder="Nhập từ khóa tìm kiếm...">
-                    <button type="submit" title="Tìm sản phẩm"><i class="fa fa-search" aria-hidden="true"></i></button>
-                </form>
-            </div>
-        </div>
-        <div id="support_online" class="hero-panel">
-            <h3 class="panel-header"><i class="fa fa-comments-o"></i>Hỗ trợ trực tuyến</h3>
-            <div class="panel-body">
-                <div class="account rows">
-                    <p class="name">Ms Trancy</p>
-                    <a target="_blank" href="skype:Sandy.nguyen14?chat" rel="nofollow">
-                        <img class="sky" src="http://nsvn.vn/template/img/skype.png" alt="Sandy.nguyen14" width="30px">
-                    </a>
-                    <div class="phone">+84 93 448 93 29</div>
-                </div>
-                <!-- account -->
-                <div class="account rows">
-                    <p class="name">Ms Thasa</p>
-                    <a target="_blank" href="skype:nguyen.thsa2301?chat" rel="nofollow">
-                        <img class="sky" src="http://nsvn.vn/template/img/skype.png" alt="nguyen.thsa2301" width="30px">
-                    </a>
-                    <div class="phone">0084 902 819 638</div>
-                </div>
-                <!-- account -->
-            </div>
-            <!-- panel-body -->
-        </div>
-        <div class="hero-panel">
-            <h3 class="panel-header">
-                <i class="fa fa-barcode"></i>
-                Featured products
-            </h3>
-            <div class="panel-body padding5">
-                <div class="vert simply-scroll-container">
-                    <div class="simply-scroll-clip">
-                        <div class="simply-scroll-list">
-                            <ul id="list-product-hot" class="simply-scroll-list">
-                                <li>
-                                    <div class="item-scroll">
-                                        <div class="product-item">
-                                            <div class="product-image">
-                                                <a href="" title="">
-                                                    <img src="http://nsvn.vn/index.php?t=ajax&amp;p=tthumb&amp;src=aHR0cDovL25zdm4udm4vdXBsb2FkL3Byb2R1Y3QvbHdwLmpwZw==&amp;w=450&amp;h=380&amp;zc=1" alt="CASHEW NUT LWP">
-                                                </a>
-                                            </div>
-                                            <a href="" title="">
-                                                <h2 class="product_name">CASHEW NUT LWP</h2>
-                                            </a>
-                                            <p class="product-des">CASHEW NUT LWP</p>
-                                            <p class="product-price">Call</p>
-                                            <a href="" class="view-more" title="">
-                                            Details
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="item-scroll">
-                                        <div class="product-item">
-                                            <div class="product-image">
-                                                <a href="http://nsvn.vn/en/san-pham/wasabi-coated-peanut-58.html" title="WASABI COATED PEANUT "><img src="http://nsvn.vn/index.php?t=ajax&amp;p=tthumb&amp;src=aHR0cDovL25zdm4udm4vdXBsb2FkL3Byb2R1Y3QvMmRjNTA3NzllMTFiMDI0NTViMGEuanBn&amp;w=450&amp;h=380&amp;zc=1" alt="WASABI COATED PEANUT "></a>
-                                            </div>
-                                            <a href="http://nsvn.vn/en/san-pham/wasabi-coated-peanut-58.html" title="WASABI COATED PEANUT ">
-                                                <h2 class="product_name">WASABI COATED PEANUT </h2>
-                                            </a>
-                                            <p class="product-des">WASABI COATED PEANUT </p>
-                                            <p class="product-price">Call</p>
-                                            <a href="http://nsvn.vn/en/san-pham/wasabi-coated-peanut-58.html" class="view-more" title="WASABI COATED PEANUT ">
-                                            Details
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>                           
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="hero-sidebar">
+            <?php get_sidebar(); ?>
         </div>
     </div>
-
 </div>
 <?php get_footer();?>
