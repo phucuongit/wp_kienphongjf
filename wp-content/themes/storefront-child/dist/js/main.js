5ae49ac82f6d1c5409c34666ab0cf88dfbc91f37
noname21062000 <noname21062000@gmail.com>
2020-02-09 22:13:46 +0700

    finish page contact

--- wp-content/themes/storefront-child/dist/js/main.js
+++ wp-content/themes/storefront-child/dist/js/main.js
@@ -9,12 +9,22 @@
             autoplay: true,
 
         });
+        /** HANDLE NAV HEADER */
         $('#menu-main-menu').prepend('<li class="nav-item"><a href="#" ><i class="fa fa-home"></i></a></li>');
         $('#menu-main-menu.navbar-nav li.nav-item.menu-item-has-children').children('a').append(' <i class="child fa fa-angle-down"></i>');
         $('#menu-main-menu.navbar-nav li.nav-item.menu-item-has-children').hover(function(){
             $(this).children('ul')[0].style.display = 'block';
         }, function(){
             $(this).children('ul')[0].style.display = 'none'; 
+        });
+        /** HANDLE STICKY NAV */
+        $(window).scroll(function(){
+            let scroll = $(window).scrollTop();
+            if (scroll >= 200) {
+                $("#masthead").addClass("sticky");
+            }else{
+                $("#masthead").removeClass("sticky");
+            }
         });
     });
 })(jQuery);
