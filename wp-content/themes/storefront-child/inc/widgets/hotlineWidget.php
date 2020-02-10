b486741b3dee6dee9990e5db3c51bb08f33a3ea8
noname21062000 <noname21062000@gmail.com>
2020-02-10 08:57:25 +0700

    create widget live support

--- wp-content/themes/storefront-child/inc/widgets/hotlineWidget.php
+++ wp-content/themes/storefront-child/inc/widgets/hotlineWidget.php
@@ -29,39 +29,54 @@ class HotlineWidget extends WP_Widget
 
     public function widget($args, $instance)
     {
-        echo '<div id="support-online" class="hero-panel">
-                <h3 class="panel-header"><i class="fa fa-comments-o"></i>Hỗ trợ trực tuyến</h3>
-            <div class="panel-body">';
-        echo '<div class="account rows">
-                        <p class="name">Ms Trancy</p>
-                        <a target="_blank" href="skype:Sandy.nguyen14?chat" rel="nofollow">
-                            <img class="sky" src="http://nsvn.vn/template/img/skype.png" alt="Sandy.nguyen14" width="30px">
-                        </a>
-                        <div class="phone">+84 93 448 93 29</div>
-                    </div>
-                    <!-- account -->
-                    <div class="account rows">
-                        <p class="name">Ms Thasa</p>
-                        <a target="_blank" href="skype:nguyen.thsa2301?chat" rel="nofollow">
-                            <img class="sky" src="http://nsvn.vn/template/img/skype.png" alt="nguyen.thsa2301" width="30px">
-                        </a>
-                        <div class="phone">0084 902 819 638</div>
-                    </div>
-                    <!-- account -->
-                </div>
-                <!-- panel-body -->
-            </div>';
+        echo $args['before_widget'];
+
+        echo $args['after_widget'];
     }
 
     // Widget Backend 
     public function form($instance)
     {
-
+        return $instance;
     }
 
     // Updating widget replacing old instances with new
     public function update($new_instance, $old_instance)
     {
-     
     }
 } // Class wpb_widget ends here
+function my_dynamic_sidebar_params($params)
+{
+
+    // get widget vars
+    $widget_name = $params[0]['widget_name'];
+    $widget_id = $params[0]['widget_id'];
+    if ($widget_name != 'Support Online Widget') {
+
+        return $params;
+    }
+
+
+    $params[0]['after_widget'] .=  '<div id="support-online" class="hero-panel">
+                    <h3 class="panel-header"><i class="fas fa-comments"></i>Hỗ trợ trực tuyến</h3>
+                <div class="panel-body">';
+    while (the_flexible_field("contact", 'widget_' . $widget_id)) :
+        if (get_row_layout() == 'new_contact') :
+            $name = get_sub_field('name', 'widget_' . $widget_id);
+            $phone_number = get_sub_field('phone_number', 'widget_' . $widget_id);
+            $params[0]['after_widget'] .=  '<div class="account rows">';
+            $params[0]['after_widget'] .=      '<p class="name">' . $name . '</p>';
+            $params[0]['after_widget'] .=      '<a target="_blank" href="skype:Sandy.nguyen14?chat" rel="nofollow">';
+            $params[0]['after_widget'] .=        '<img class="sky" src="http://nsvn.vn/template/img/skype.png" alt="Sandy.nguyen14" width="30px">';
+            $params[0]['after_widget'] .=      '</a>';
+            $params[0]['after_widget'] .=      '<div class="phone">' . $phone_number . '</div>';
+            $params[0]['after_widget'] .=  '</div>';
+        endif;
+    endwhile;
+
+    $params[0]['after_widget'] .= '</div>';
+    $params[0]['after_widget'] .= '</div>';
+
+    return $params;
+}
+add_filter('dynamic_sidebar_params', 'my_dynamic_sidebar_params');
