84ea022ab7e81ba0018d28e17d933afab7c04c10
ancao2504 <pullnolovesmile@gmail.com>
2020-02-12 23:20:23 +0700

    add theme opntions, custom STMP email, add images form email woo

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -229,3 +229,60 @@ add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_arg
 	return $args;
 }
 
+/* THIẾT LẬP THEME OPTIONS */
+
+if( function_exists('acf_add_options_page') ) {
+	acf_add_options_page(array(
+		'page_title' 	=> 'Thiết lập giao diện', 
+		'menu_title'	=> 'Thiết lập giao diện', 
+		'menu_slug' 	=> 'theme-settings', 
+		'capability'	=> 'edit_posts',
+		'redirect'	=> false
+	));
+}
+
+/* SỬ DỤNG STMP GMAIL */
+add_action( 'phpmailer_init', function( $phpmailer ) {
+    $contentText = get_field('kp_from_name_text_gmail','option');
+    $userName = get_field('kp_username_gmail','option');
+    $passWord = get_field('kp_password_app_gmail','option');
+    if ( !is_object( $phpmailer ) )
+    $phpmailer = (object) $phpmailer;
+    $phpmailer->Mailer     = 'smtp';
+    $phpmailer->Host       = 'smtp.gmail.com';
+    $phpmailer->SMTPAuth   = 1;
+    $phpmailer->Port       = 587;
+    $phpmailer->Username   = ''.$userName.'';
+    $phpmailer->Password   = ''.$passWord.'';
+    $phpmailer->SMTPSecure = 'TLS';
+    $phpmailer->From       = ''.$userName.'';
+    $phpmailer->FromName   = ''.$contentText.'';
+});
+
+/* THÊM HÌNH ẢNH SẢN PHẨM VÀO FORM GỬI MAIL */
+add_filter( 'woocommerce_email_order_items_args', 'iconic_email_order_items_args', 10, 1 );
+function iconic_email_order_items_args( $args ) {
+    $args['show_image'] = true;
+    return $args;
+}
+
+/* THEME OPTIONS INFOMATION COMPANMY - ADDRESS ...*/
+function createACCompany() {
+	$company = get_field('kp_company','option');
+	return $company;
+}
+
+function createACAddress() {
+	$address = get_field('kp_address','option');
+	return $address;
+}
+
+function createACEmail() {
+	$email = get_field('kp_email','option');
+	return $email;
+}
+
+function createACPhone() {
+	$phone = get_field('kp_phone','option');
+	return $phone;
+}
